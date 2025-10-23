8b189c85b6908cf86908d8d68658c5abd7d17af5
Sifat2245 <mdsifat1015@gmail.com>
2025-10-23 10:13:52 +0600

    feat: designed the appoinments page as figma. show the data in table added action buttons

---
+++ src/app/dashboard/appointments/book-an-appointment/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BookAnAppointmentPage = () => {
+    return (
+        <div>
+            <h1>fill the appointment form</h1>
+        </div>
+    );
+};
+
+export default BookAnAppointmentPage;
--- src/app/dashboard/appointments/page.tsx
+++ src/app/dashboard/appointments/page.tsx
@@ -1,15 +1,60 @@
+import AppointmentTable from '@/app/pages/dashboard/appointments/AppointmentTable/AppointmentTable'
+import CommonButton from '@/common/CommonButton'
 import { getAllAppointment } from '@/services/dashboard/appointment/appointment'
 import { TAppointments } from '@/Types'
+import Link from 'next/link'
 import React from 'react'
+import Image from "next/image";
+import AppointmentImage from '@/assets/images/OBJECTS.png'
 
-const BookAppointmentPage = async() => {
+const BookAppointmentPage = async () => {
 
   const data = await getAllAppointment()
-  const appointmentData : TAppointments[] = data instanceof Error ? [] : data;
+  const appointmentData: TAppointments[] = data instanceof Error ? [] : data;
+
+  //no data show text and button
+  if (!appointmentData || appointmentData.length === 0) {
+    return (
+      <div className="pt-32">
+        <div className='flex justify-center h-auto max-w-md mx-auto'>
+          <Image src={AppointmentImage} alt="object image"></Image>
+        </div>
+        <div className='text-center mt-6'>
+          <h1 className='text-4xl font-medium mb-4'>No Appointments Yet</h1>
+          <p className='text-[#677489] tracking-wider'>You don’t have any scheduled consultations. Book your first appointment to get started.</p>
+
+          <Link href={'/dashboard/appointments/book-an-appointment'}>
+            <div className='flex justify-center items-center mt-8'>
+              <CommonButton variant='primary' className='rounded-2xl!'>Book An Appointment</CommonButton>
+            </div>
+          </Link>
+        </div>
+      </div>
+    )
+  }
 
   console.log(appointmentData);
   return (
-    <div>page</div>
+    <div>
+      <div className='bg-[#FBFBFB] p-8 rounded-2xl'>
+        <div className=' flex justify-between items-center'>
+          <div>
+            <h1 className='text-4xl font-semibold mb-2'>Upcoming  Appointments</h1>
+            <p className='text-[#677489] tracking-widest'>Easily schedule tax or accounting chats from your dashboard to manage your finances!</p>
+          </div>
+          <div>
+            <Link href={'/dashboard/appointments/book-an-appointment'}>
+              <CommonButton variant='primary' className='font-normal! rounded-2xl!'>Book An Appointment</CommonButton>
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      {/* table */}
+      <div className='mt-10'>
+        <AppointmentTable appointmentData={appointmentData} />
+      </div>
+    </div>
   )
 }
 
